3b1400764fe8bda27f6e4b80890e64e8a3f27e81
yusuf7861 <yjamal710@gmail.com>
2023-04-06 14:41:07 +0530

    ..

---
+++ Chapter-18_Console/Examples.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main()
+{
+    char firstname1[ ] = "Sandy";
+    char surname1[ ] = "Malya";
+    char firstname2[ ] = "YusufJamal";
+    char surname2[ ] = "Gurubaxmi";
+
+    printf("%20s%20s\n", firstname1, surname1);
+    printf("%20s%20s\n", firstname2, surname2);
+    return 0;
+}
+
+// following program shows the usage of escape sequence
+#include<stdio.h>
+int main()
+{
+    return 0;
+}
