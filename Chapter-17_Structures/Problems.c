ea2f87fe8e329b498039fea55238a6181c891ac4
yusuf7861 <yjamal710@gmail.com>
2023-04-06 14:44:07 +0530

    console i/o

--- Chapter-17_Structures/Problems.c
+++ Chapter-17_Structures/Problems.c
@@ -107,14 +107,131 @@
 //     printf("%s %s %d\n", pb->name, pb->author, pb->pages);
 // }
 
+// #include<stdio.h>
+// int main()
+// {
+//     struct emp
+//     {
+//         int a; char ch; float s;
+//     };
+//     struct emp e ;
+//     printf("%u %u %u\n", &e.a, &e.ch, &e.s);
+//     return 0;
+// }
+
+// It is possible to create additional variables of the same type after typedefing struct Student to "Student"
+// #include<stdio.h>
+// #include<string.h>
+// typedef struct Student 
+// {
+//     char f_name[20];
+//     char l_name[20];
+//     long int enrollment;
+//     double grade_point;
+// } Student;
+
+// int main()
+// {
+//     Student S1, S2;
+//     strcpy(S1.f_name, "Yusuf");
+//     strcpy(S1.l_name, "Jamal");
+//     S1.enrollment = 6395;
+//     S1.grade_point = 60.8;
+
+//     // Assign values to member of S2
+//     strcpy(S2.f_name, "Komal");
+//     strcpy(S2.l_name, "Saxena");
+//     S2.enrollment = 6200;
+//     S2.grade_point = 90.8;
+
+//     // Print the values of S1
+//     printf("\nStudent details: ");
+//     printf("\nFirst name: %s", S1.f_name);
+//     printf("\nLast name: %s", S1.l_name);
+//     printf("\nEnrollment No.: %ld", S1.enrollment);
+//     printf("\nGrade point average: %5.2f\n", S1.grade_point);
+
+//     // Print values of S2
+//     printf("\nStudent details: ");
+//     printf("\nFirst name: %s", S2.f_name);
+//     printf("\nLast name: %s", S2.l_name);
+//     printf("\nEnrollment No.: %ld", S2.enrollment);
+//     printf("\nGrade point average: %5.2f", S2.grade_point);
+//     return 0;
+// }
+
+//! Problem(17.1)
+//* A stack is a data structure in which addition of new element or deletion of existing element always takes place at the same end known as 'top' of stack. Write a program to implement a stack using a linked list.
+
+// Implementation of stack using a linked list
+// #include<stdio.h>
+// #include<stdlib.h>
+// struct node
+// {
+//     int data; 
+//     struct node *link;
+// };
+// void push (struct node **s, int item); // function definition 
+// int pop (struct node **s);  // function definition
+// int main()
+// {
+//     struct node *top;
+//     int t, i, item;
+//     top = NULL;
+//     push(&top, 45);
+//     push(&top, 28);
+//     push(&top, 63);
+//     push(&top, 55);
+
+//     item = pop (&top);
+//     printf("Popped : %d\n", item);
+//     item = pop (&top);
+//     printf("Popped : %d\n", item);
+//     return 0;
+// }
+// void push (struct node **s, int item)
+// {
+//     struct node *q;
+//     q = (struct node *) malloc(sizeof(struct node));
+//     q -> data = item;
+//     q -> link = *s;
+//     *s = q;
+// }
+
+// int pop (struct node **s)
+// {
+//     int item;
+//     struct node *q;
+//     if(*s == NULL)
+//         printf("Stack is empty\n");
+//     else
+//     {
+//         q = *s;
+//         item = q -> data;
+//         *s = q ->link;
+//         free ( q );
+//         return (item);
+//     }
+// }
+
+//! Problems(17.2)
+// In a data structure called queue the addition of new element takes place at the end (called 'rear' of queue), whereas deletion takes place at the other end (called 'front' of queue). write a program to implement a queue using a linked list.
 #include<stdio.h>
+#include<stdlib.h>
+struct queue 
+{
+    int item;
+    struct queue *link;
+};
+struct queue *rear, *front;
+void add (int item);
+int del_queue();
 int main()
 {
-    struct emp
-    {
-        int a; char ch; float s;
-    };
-    struct emp e ;
-    printf("%u %u %u\n", &e.a, &e.ch, &e.s);
-    return 0;
+    int item;
+    rear = front = NULL;
+    add(10); add(20); add(30);
+    add(40); add(50); add(60);
+    item = del_queue();
+    printf("Deleted Item = %d\n", item);
 }
