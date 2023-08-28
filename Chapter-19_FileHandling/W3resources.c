92503530e4b45b4832ab952a527b235efecd5157
yusuf7861 <yjamal710@gmail.com>
2023-08-28 19:16:11 +0530

    exercise-19 added

--- Chapter-19_FileHandling/W3resources.c
+++ Chapter-19_FileHandling/W3resources.c
@@ -57,41 +57,41 @@
 
 
 //* Write a program in C to write multiple lines to a text file.
-#include <stdio.h>
+// #include <stdio.h>
 
-int main ()
-{
-  FILE * fptr;
-  int i,n;
-  char str[100];
-  char fname[20]="test.txt";
-  char str1;
+// int main ()
+// {
+//   FILE * fptr;
+//   int i,n;
+//   char str[100];
+//   char fname[20]="test.txt";
+//   char str1;
   
-    printf("\n\n Write multiple lines in a text file and read the file :\n");
-	printf("------------------------------------------------------------\n");   
-	printf(" Input the number of lines to be written : ");
-	scanf("%d", &n);
-	printf("\n :: The lines are ::\n");
-	fptr = fopen (fname,"w"); 
-	for(i = 0; i < n+1;i++)
-		{
-		fgets(str, sizeof str, stdin);
-		fputs(str, fptr);
-		}
-  fclose (fptr);
-/*-------------- read the file -------------------------------------*/
-	fptr = fopen (fname, "r");  
-	printf("\n The content of the file %s is  :\n",fname);
-	str1 = fgetc(fptr);
-	while (str1 != EOF)
-		{
-			printf ("%c", str1);
-			str1 = fgetc(fptr);
-		}
-    printf("\n\n");
-    fclose (fptr);
-    return 0;
-}
+//     printf("\n\n Write multiple lines in a text file and read the file :\n");
+// 	printf("------------------------------------------------------------\n");   
+// 	printf(" Input the number of lines to be written : ");
+// 	scanf("%d", &n);
+// 	printf("\n :: The lines are ::\n");
+// 	fptr = fopen (fname,"w"); 
+// 	for(i = 0; i < n+1;i++)
+// 		{
+// 		fgets(str, sizeof str, stdin);
+// 		fputs(str, fptr);
+// 		}
+//   fclose (fptr);
+// /*-------------- read the file -------------------------------------*/
+// 	fptr = fopen (fname, "r");  
+// 	printf("\n The content of the file %s is  :\n",fname);
+// 	str1 = fgetc(fptr);
+// 	while (str1 != EOF)
+// 		{
+// 			printf ("%c", str1);
+// 			str1 = fgetc(fptr);
+// 		}
+//     printf("\n\n");
+//     fclose (fptr);
+//     return 0;
+// }
 
 
 //* Write a program in C to read the file and store the lines in an array.
@@ -117,7 +117,7 @@ int main ()
 //     fptr = fopen(fname, "r");
 //     while (fgets(line[i], LSIZ, fptr))
 //     {
-        
+		
 //     }
     
 // }
